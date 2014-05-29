1461ec6ec84e9d8348c0426d4c063b7d6a5a9bb1
Radek Pazdera <radek@kano.me>
2014-05-29 14:18:00 +0100

    Adding a simple Python logger with bindings for bash

---
+++ bash/logger.sh
@@ -0,0 +1,51 @@
+# bash wrapper for kano.logger
+#
+# Copyright (C) 2014 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
+# Usage:
+#
+# . /usr/share/kano-toolset/logger.sh
+#
+# set_app_name "make-minecraft"
+#
+# logger_write "Error! Error!"
+
+CONF="/etc/debug"
+APP_NAME="$0"
+
+function _is_logger_enabled
+{
+    # The result is stored in a var, so this check
+    # should happen just once per application runtime
+    if [ -z "$LOGGER_ENABLED" ]; then
+        if [ -e "$CONF" ]; then
+            export LOGGER_ENABLED=1
+        else
+            export LOGGER_ENABLED=0
+        fi
+    fi
+}
+
+function logger_set_app_name
+{
+    export APP_NAME="$1"
+}
+
+function logger_write
+{
+    _is_logger_enabled
+
+    if [ "$LOGGER_ENABLED" -eq 1 ]; then
+        python <<EOF
+import kano.logger as logger
+
+# We already know that logging is enabled, so we force-enable it here.
+# Therefore, python will not check the CONF file again.
+logger.enable()
+
+logger.set_app_name("$APP_NAME")
+logger.write("$1")
+EOF
+    fi
+}
---
+++ kano/logger.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+# kano.logger
+#
+# Copyright (C) 2014 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
+
+import os
+import sys
+import json
+import time
+
+CONF = "/etc/debug"
+SYSTEM_LOGS_DIR = "/var/log/kano/"
+USER_LOGS_DIR = "~/.logs/"
+
+logging_enabled = False
+log_file = None
+app_name = None
+
+
+def enable():
+    global logging_enabled
+    logging_enabled = True
+
+
+def disable():
+    global logging_enabled, log_file
+    if log_file:
+        log_file.close()
+        log_file = None
+
+    logging_enabled = False
+
+
+def set_app_name(name):
+    global app_name
+    app_name = os.path.basename(name.strip()).lower().replace(" ", "_")
+    print "`" + app_name+"`"
+    if log_file != None:
+        _init_log_file()
+
+
+def write(msg, data=None):
+    if logging_enabled == None:
+        if os.path.exists(CONF):
+            enable()
+
+    if logging_enabled:
+        if log_file == None:
+            _init_log_file()
+
+        if app_name == None:
+            set_app_name(sys.argv[0])
+
+        log = data.copy() if data != None else {}
+        log["message"] = msg
+        log["time"] = time.time()
+        log["pid"] = os.getpid()
+        log_file.write("{}\n".format(json.dumps(log)))
+
+
+def _init_log_file():
+    global log_file
+
+    if log_file != None:
+        log_file.close()
+
+    logs_dir = os.path.expanduser(USER_LOGS_DIR)
+    if os.getuid() == 0:
+        logs_dir = os.path.expanduser(SYSTEM_LOGS_DIR)
+
+    if not os.path.exists(logs_dir):
+        os.makedirs(logs_dir)
+
+    log_file = open("{}/{}.log".format(logs_dir, app_name), "a")
