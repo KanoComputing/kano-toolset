d9705a190bdd1e6be7be2bde34bebf46588a0c07
Radek Pazdera <radek@kano.me>
2014-05-30 14:41:58 +0100

    logging: Major refactoring
    - ENV vars are used instead of a file to set modes
    - Apart from logging mode, there's now a debugging mode
    - Added colours!
    - Added levels

--- bash/logger.sh
+++ bash/logger.sh
@@ -11,21 +11,7 @@
 #
 # logger_write "Error! Error!"
 
-CONF="/etc/debug"
 APP_NAME="$0"
-
-function _is_logger_enabled
-{
-    # The result is stored in a var, so this check
-    # should happen just once per application runtime
-    if [ -z "$LOGGER_ENABLED" ]; then
-        if [ -e "$CONF" ]; then
-            export LOGGER_ENABLED=1
-        else
-            export LOGGER_ENABLED=0
-        fi
-    fi
-}
 
 function logger_set_app_name
 {
@@ -34,20 +20,24 @@ function logger_set_app_name
 
 function logger_write
 {
-    _is_logger_enabled
+    local msg=$1
+    local level=$2
 
-    if [ "$LOGGER_ENABLED" -eq 1 ]; then
+    local kwargs=""
+    if [ -n "$level" ]; then
+        kwargs="$kwargs, level=\"$level\""
+    fi
+
+    # Optimisation: Don't launch python unless logging is enabled
+    if ( [ -n "$LOG_LEVEL" ] && [ "$LOG_LEVEL" != "none" ] ) \
+        || ( [ -n "$DEBUG_LEVEL" ] && [ "$DEBUG_LEVEL" != "none" ] ); then
         python <<EOF
 import kano.logger as logger
-
-# We already know that logging is enabled, so we force-enable it here.
-# Therefore, python will not check the CONF file again.
-logger.enable()
 
 logger.pid = $$
 logger.set_app_name("$APP_NAME")
 
-logger.write("$1")
+logger.write("$msg" $kwargs)
 EOF
     fi
 }
--- kano/logger.py
+++ kano/logger.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # kano.logger
 #
 # Copyright (C) 2014 Kano Computing Ltd.
@@ -8,61 +6,96 @@
 
 import os
 import sys
+import re
 import json
 import time
+from kano.colours import decorate_string, decorate_with_preset
 
-CONF = "/etc/debug"
+LOG_ENV = "LOG_LEVEL"
+DEBUG_ENV = "DEBUG_LEVEL"
 SYSTEM_LOGS_DIR = "/var/log/kano/"
 USER_LOGS_DIR = os.path.expanduser("~/.kano-logs/")
 
-logging_enabled = False
+
+def normalise_level(level):
+    if level == None:
+        return "none"
+
+    level = level.lower()
+    if level == "none"[0:len(level)]:
+        return "none"
+    elif level == "error"[0:len(level)]:
+        return "error"
+    elif level == "warning"[0:len(level)]:
+        return "warning"
+    elif level == "info"[0:len(level)]:
+        return "info"
+    elif level == "debug"[0:len(level)]:
+        return "debug"
+
+
+def get_log_level():
+    return normalise_level(os.environ.get(LOG_ENV))
+
+
+def get_debug_level():
+    return normalise_level(os.environ.get(DEBUG_ENV))
+
+
+levels = {
+    "none": 0,
+    "error": 1,
+    "warning": 2,
+    "info": 3,
+    "debug": 4
+}
+log_level = levels[get_log_level()]
+debug_level = levels[get_debug_level()]
+
 log_file = None
 app_name = None
 pid = os.getpid()
 
 
-def enable():
-    global logging_enabled
-    logging_enabled = True
-
-
-def disable():
-    global logging_enabled, log_file
-    if log_file:
+def set_app_name(name):
+    global app_name, log_file
+    app_name = os.path.basename(name.strip()).lower().replace(" ", "_")
+    if log_file != None:
         log_file.close()
         log_file = None
 
-    logging_enabled = False
 
+def write(msg, **kwargs):
+    lname = "info"
+    if "level" in kwargs:
+        lname = normalise_level(kwargs["level"])
 
-def set_app_name(name):
-    global app_name
-    app_name = os.path.basename(name.strip()).lower().replace(" ", "_")
-    if log_file != None:
-        _init_log_file()
+    level = levels[lname]
 
-def set_pid(p):
-    global pid
-    pid = p
-
-
-def write(msg, data=None):
-    if logging_enabled == None:
-        if os.path.exists(CONF):
-            enable()
-
-    if logging_enabled:
-        if log_file == None:
-            _init_log_file()
-
+    log = {}
+    if level > 0 and (level <= log_level or level <= debug_level):
         if app_name == None:
             set_app_name(sys.argv[0])
 
-        log = data.copy() if data != None else {}
-        log["message"] = msg
         log["time"] = time.time()
         log["pid"] = pid
-        log_file.write("{}\n".format(json.dumps(log)))
+        log.update(kwargs)
+        log["message"] = msg
+        log["level"] = lname
+
+        if level <= log_level:
+            if log_file == None:
+                _init_log_file()
+
+            log_file.write("{}\n".format(json.dumps(log)))
+
+        if level <= debug_level:
+            print "{}[{}] {} {}".format(
+                app_name,
+                decorate_string(pid, "yellow"),
+                decorate_with_preset(log["level"], log["level"]),
+                log["message"]
+            )
 
 
 def _init_log_file():
