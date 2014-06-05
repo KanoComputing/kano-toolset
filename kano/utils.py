9e82d1179563eec45378cb791c908ee37267a66d
Alex Simon <alejandro@kano.me>
2014-06-05 13:06:50 +0100

    Applying Albert's fix

--- kano/utils.py
+++ kano/utils.py
@@ -275,5 +275,5 @@ def play_sound(audio_file):
     except Exception:
         pass
 
-    _, _, rc = run_cmd(cmd + audio_file + " > null")
+    _, _, rc = run_cmd(cmd + audio_file + " > /dev/null 2>&1)
     return rc == 0
