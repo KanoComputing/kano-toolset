a6a39d4f6822fe67fe52d2b09d47f8711a5cc222
Zsolt Ero <zsolt.ero@gmail.com>
2014-07-01 15:15:31 +0100

    moved sub-functions into main functions, removed rawdata as member

--- kano/network.py
+++ kano/network.py
@@ -28,191 +28,176 @@ class IWList():
         For testing, <iwlist> can point to a file name which contains
         the output of a "iwlist" command, very useful to test remote complex network neighbourhoods.
         '''
-        self.rawdata = ""
         self.data = {}
         self.interface = interface
         self.refresh(iwlist=iwlist)
 
     def refresh(self, iwlist=None):
+
+        def getRawData(interface, iwlist=None):
+            '''
+            Runs iwlist and gets WiFi data in a string
+            Developped, tested with Wireless Extension v29 English translation, Nov 2007
+            If iwlist points to a file, you can mimic iwlist from a response file for testing
+            '''
+            outdata = ''
+            retries = 3
+
+            # Make sure the wlan interface is up, otherwise the network scan will not proceed
+            os.system('ifconfig %s up' % interface)
+            if iwlist:
+                outdata = open(iwlist, 'r').read()
+            else:
+                # Contemplate those seldom cases where the dongle driver returns an empty list
+                while len(outdata) < 80 and retries:
+                    cstring = "iwlist " + interface + " scan 2>/dev/nul"
+                    outdata = os.popen(cstring).read()
+                    retries -= 1
+
+            return outdata
+
+        def parseRawData(rawdata):
+            # Parses a string containing the data printed by iwlist
+            # Pre-condition: rawdata is not empty
+            rawdatas = rawdata.split("\n")
+            # Strip blanks
+            # Let's separate by cells
+            cellDataL = []
+            #currentCell = None
+            for s in rawdatas:
+                # If new cell:
+                if s.lstrip().startswith("Cell "):
+                    # log.debug("parseRawData: new cell")
+                    cellDataL.append([])
+                if len(cellDataL) > 0 and len(s) > 0:
+                    cellDataL[len(cellDataL) - 1].append(s)
+            # Data is separated by cells, now we'll parse each cell's data
+            parsedCellData = {}
+            for s in cellDataL:
+                if s is not None:
+                    (cellNumber, cellData) = parseCellData("\n".join(s))
+                    parsedCellData[cellNumber] = cellData
+            #log.debug("parseRawData: parsed "+str(len(cellDataL))+" cells")
+            return parsedCellData
+
+        def parseCellData(rawCellData):
+            # Parses a string containing raw cell data
+            # @return a tuble containing the cell's number and a dictionary with the data
+
+            def getCellExtra(s):
+                s = s.split(":")
+                if len(s) > 2:
+                    ret = ":".join(s[1:]).strip()
+                else:
+                    ret = s[1].strip()
+                return ret
+
+            def getCellIE(s):
+                s = s.split(":")
+                if len(s) > 2:
+                    ret = ":".join(s[1:]).strip()
+                else:
+                    ret = s[1].strip()
+                return ret
+
+            def getCellNumber(s):
+                return s.strip().split(" ")[1]
+
+            def getCellFrequency(s):
+                s = s.split(":")[1]
+                return s.strip().split(" ")[0]
+
+            def getCellChannel(s):
+                return s.strip().split(" ")[3][0:-1]
+
+            def getCellEncryption(s):
+                return s.strip().split(":")[1]
+
+            def getCellSignal(s):
+                s = s.split("Signal level=")[1]
+                return s.strip().split(" ")[0]
+
+            def getCellNoise(s):
+                try:
+                    s = s.split("Noise level:")[1]
+                    return s.strip().split(" ")[0]
+                except:
+                    return 0
+
+            def getCellQuality(s):
+                s = s.split("=")[1]
+                return s.strip().split(" ")[0]
+
+            def getCellMAC(s):
+                return s.strip().split(" ")[4]
+
+            def getCellESSID(s):
+                return s.strip().split(":\"")[1][0:-1]
+
+            def getCellProtocol(s):
+                return s.strip().split(":")[1][-1]
+
+            def getCellMode(s):
+                return s.strip().split(":")[1]
+
+            splitRawData = rawCellData.split("\n")
+            cellData = {}
+            for s in splitRawData:
+                if s.strip().startswith("Cell "):
+                    cellData["Number"] = getCellNumber(s)
+                    cellData["MAC"] = getCellMAC(s)
+                if s.strip().startswith("ESSID:\""):
+                    cellData["ESSID"] = getCellESSID(s)
+                if s.strip().startswith("Protocol:"):
+                    cellData["Protocol"] = getCellProtocol(s)
+                if s.strip().startswith("Mode:"):
+                    cellData["Mode"] = getCellMode(s)
+                if s.strip().startswith("Mode:"):
+                    cellData["Mode"] = getCellMode(s)
+                if s.strip().startswith("Frequency:"):
+                    cellData["Frequency"] = getCellFrequency(s)
+                    cellData["Channel"] = getCellChannel(s)
+                if s.strip().startswith("Quality="):
+                    cellData["Quality"] = getCellQuality(s)
+                    cellData["Signal"] = getCellSignal(s)
+                    cellData["Noise"] = getCellNoise(s)
+                if s.strip().startswith("Encryption key:"):
+                    cellData["Encryption"] = getCellEncryption(s)
+
+                if s.strip().startswith("IE"):
+                    try:
+                        ie = cellData["IE"]
+                    except KeyError:
+                        ie = []
+                    ie.append(getCellIE(s))
+                    cellData["IE"] = ie
+
+                if s.strip().startswith("Extra:"):
+                    try:
+                        extra = cellData["Extra"]
+                    except KeyError:
+                        extra = []
+                    extra.append(getCellExtra(s))
+                    cellData["Extra"] = extra
+
+            return cellData["Number"], cellData
+
         # Get raw data as a string
-        self.rawdata = self.getRawData(self.interface, iwlist)
+        rawdata = getRawData(self.interface, iwlist)
         # Parse raw data into a dictionary
-        if self.rawdata is not None and len(self.rawdata.strip()):
-            self.data = self.parseRawData(self.rawdata)
-
-    def getRawData(self, interface, iwlist=None):
-        '''
-        Runs iwlist and gets WiFi data in a string
-        Developped, tested with Wireless Extension v29 English translation, Nov 2007
-        If iwlist points to a file, you can mimic iwlist from a response file for testing
-        '''
-        outdata = ''
-        retries = 3
-
-        # Make sure the wlan interface is up, otherwise the network scan will not proceed
-        os.system('ifconfig %s up' % interface)
-        if iwlist:
-            outdata = open(iwlist, 'r').read()
-        else:
-            # Contemplate those seldom cases where the dongle driver returns an empty list
-            while len(outdata) < 80 and retries:
-                cstring = "iwlist " + interface + " scan 2>/dev/nul"
-                outdata = os.popen(cstring).read()
-                retries -= 1
-
-        return outdata
-
-    def parseRawData(self, rawdata):
-        # Parses a string containing the data printed by iwlist
-        # Pre-condition: rawdata is not empty
-        rawdatas = rawdata.split("\n")
-        # Strip blanks
-        # Let's separate by cells
-        cellDataL = []
-        #currentCell = None
-        for s in rawdatas:
-            # If new cell:
-            if s.lstrip().startswith("Cell "):
-                # log.debug("parseRawData: new cell")
-                cellDataL.append([])
-            if len(cellDataL) > 0 and len(s) > 0:
-                cellDataL[len(cellDataL) - 1].append(s)
-        # Data is separated by cells, now we'll parse each cell's data
-        parsedCellData = {}
-        for s in cellDataL:
-            if s is not None:
-                (cellNumber, cellData) = self.parseCellData("\n".join(s))
-                parsedCellData[cellNumber] = cellData
-        #log.debug("parseRawData: parsed "+str(len(cellDataL))+" cells")
-        return parsedCellData
-
-    def printData(self):
-        # Debugging print
-        for s in self.data:
-            print s, self.data[s]
-
-    def parseCellData(self, rawCellData):
-        # Parses a string containing raw cell data
-        # @return a tuble containing the cell's number and a dictionary with the data
-
-        splitRawData = rawCellData.split("\n")
-        cellData = {}
-        for s in splitRawData:
-            if s.strip().startswith("Cell "):
-                cellData["Number"] = self.getCellNumber(s)
-                cellData["MAC"] = self.getCellMAC(s)
-            if s.strip().startswith("ESSID:\""):
-                cellData["ESSID"] = self.getCellESSID(s)
-            if s.strip().startswith("Protocol:"):
-                cellData["Protocol"] = self.getCellProtocol(s)
-            if s.strip().startswith("Mode:"):
-                cellData["Mode"] = self.getCellMode(s)
-            if s.strip().startswith("Mode:"):
-                cellData["Mode"] = self.getCellMode(s)
-            if s.strip().startswith("Frequency:"):
-                cellData["Frequency"] = self.getCellFrequency(s)
-                cellData["Channel"] = self.getCellChannel(s)
-            if s.strip().startswith("Quality="):
-                cellData["Quality"] = self.getCellQuality(s)
-                cellData["Signal"] = self.getCellSignal(s)
-                cellData["Noise"] = self.getCellNoise(s)
-            if s.strip().startswith("Encryption key:"):
-                cellData["Encryption"] = self.getCellEncryption(s)
-
-            if s.strip().startswith("IE"):
-                try:
-                    ie = cellData["IE"]
-                except KeyError:
-                    ie = []
-                ie.append(self.getCellIE(s))
-                cellData["IE"] = ie
-
-            if s.strip().startswith("Extra:"):
-                try:
-                    extra = cellData["Extra"]
-                except KeyError:
-                    extra = []
-                extra.append(self.getCellExtra(s))
-                cellData["Extra"] = extra
-
-        return cellData["Number"], cellData
-
-    def getCellExtra(self, s):
-        s = s.split(":")
-        if len(s) > 2:
-            ret = ":".join(s[1:]).strip()
-        else:
-            ret = s[1].strip()
-        return ret
-
-    def getCellIE(self, s):
-        s = s.split(":")
-        if len(s) > 2:
-            ret = ":".join(s[1:]).strip()
-        else:
-            ret = s[1].strip()
-        return ret
-
-    def getCellBitRates(self, s, rawdatas):
-        # Pre-condition: s is in rawdatas, and bit rates are described in 3 lines
-        ixBitRate = rawdatas.index(s)
-        rawBitRate = rawdatas[ixBitRate].split(":")[1].strip() + "; " + rawdatas[ixBitRate + 1].strip() + "; " + \
-            rawdatas[ixBitRate + 2].strip()
-        return rawBitRate
-
-    def getCellNumber(self, s):
-        return s.strip().split(" ")[1]
-
-    def getCellFrequency(self, s):
-        s = s.split(":")[1]
-        return s.strip().split(" ")[0]
-
-    def getCellChannel(self, s):
-        return s.strip().split(" ")[3][0:-1]
-
-    def getCellEncryption(self, s):
-        return s.strip().split(":")[1]
-
-    def getCellSignal(self, s):
-        s = s.split("Signal level=")[1]
-        return s.strip().split(" ")[0]
-
-    def getCellNoise(self, s):
-        try:
-            s = s.split("Noise level:")[1]
-            return s.strip().split(" ")[0]
-        except:
-            return 0
-
-    def getCellQuality(self, s):
-        s = s.split("=")[1]
-        return s.strip().split(" ")[0]
-
-    def getCellMAC(self, s):
-        return s.strip().split(" ")[4]
-
-    def getCellESSID(self, s):
-        return s.strip().split(":\"")[1][0:-1]
-
-    def getCellProtocol(self, s):
-        return s.strip().split(":")[1][-1]
-
-    def getCellMode(self, s):
-        return s.strip().split(":")[1]
-
-    def getData(self):
-        return self.data
-
-    def sortNetworks(self, adict):
-        x, z = adict['quality'].split('/')
-        factor = int(x) / float(z)
-        return factor
+        if rawdata:
+            self.data = parseRawData(rawdata)
 
     def getList(self, unsecure=False, first=False, debug=False):
         '''
         Return a comfortable list of wireless networks
         sorted by signal strength (strongest first)
         '''
+
+        def sortNetworks(adict):
+            x, z = adict['quality'].split('/')
+            factor = int(x) / float(z)
+            return factor
 
         if debug:
             import pprint
@@ -257,7 +242,7 @@ class IWList():
                     wnet['encryption'] = enc
                     self.iwnets.append(wnet)
 
-        self.iwnets = sorted(self.iwnets, key=self.sortNetworks, reverse=True)
+        self.iwnets = sorted(self.iwnets, key=sortNetworks, reverse=True)
         if first and len(self.iwnets) > 1:
             return [self.iwnets[0]]
         else:
