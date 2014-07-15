3c0145c3132cc04f35cb15601d0cfc0c3e7589f5
carolineclark <caroline@kano.me>
2014-07-15 10:17:19 +0100

    Description is not packed into container if is empty

--- kano/gtk3/heading.py
+++ kano/gtk3/heading.py
@@ -15,30 +15,35 @@ from kano.paths import common_css_dir
 class Heading():
     def __init__(self, title, description):
 
-        self.title = Gtk.Label(title)
-        self.description = Gtk.Label(description)
-
-        # Table
-        self.container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
-        self.container.pack_start(self.title, False, False, 6)
-        self.container.pack_start(self.description, False, False, 0)
-
         cssProvider = Gtk.CssProvider()
         cssProvider.load_from_path(common_css_dir + "/heading.css")
         styleContext = Gtk.StyleContext()
         styleContext.add_provider(cssProvider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
 
+        self.title = Gtk.Label(title)
         self.title_style = self.title.get_style_context()
         self.title_style.add_provider(cssProvider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
         self.title_style.add_class('title')
 
-        self.description_style = self.description.get_style_context()
-        self.description_style.add_provider(cssProvider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
-        self.description_style.add_class('description')
+        self.container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
+        self.container.pack_start(self.title, False, False, 6)
+
+        if not description == "":
+            self.description = Gtk.Label(description)
+
+            self.description_style = self.description.get_style_context()
+            self.description_style.add_provider(cssProvider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
+            self.description_style.add_class('description')
+
+            self.container.pack_start(self.description, False, False, 0)
 
     def set_text(self, title, description):
         self.title.set_text(title)
-        self.description.set_text(description)
+        if getattr(self, 'description'):
+            self.description.set_text(description)
 
     def get_text(self):
-        return [self.title.get_text(), self.description.get_text()]
+        if getattr(self, 'description'):
+            return [self.title.get_text(), self.description.get_text()]
+        else:
+            return [self.title.get_text(), ""]
