9592e61161ca4d001435039fd3d218d967838511
Alex Burr <alex.burr@kano.me>
2015-02-06 18:04:55 +0000

    First cut at splash implementation. Currently, the splash window is does not remain on top. Terminating it early is not tested. It also doesn't seem to start all that fast, maybe there is some delay in kano-launcher?

---
+++ kano-splash/get-start-time.h
@@ -0,0 +1,19 @@
+/* get-start-time.h
+ *
+ * Copyright (C) 2015 Kano Computing Ltd.
+ * License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * 
+ * This file exports a function for extracting a process starttime from proc.
+ * The idea is to avoid sending signals to a process which has obtained the same pid
+ * as the one you intended to send it to.
+ */
+
+#ifndef GET_START_TIME_H
+#define GET_START_TIME_H
+/* 
+ * Extract process start time from /proc/<pid>/stat 
+ * If anything goes wrong, return 0 
+ */
+unsigned long long get_process_start_time(pid_t pid);
+
+#endif /*GET_START_TIME_H*/
