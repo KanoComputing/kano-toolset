d3aaeb1bc6491964fa480e2bd93ecf962b40d958
Costas Voloudakis <costas@kano.me>
2015-02-13 11:12:37 +0000

    Clear Profiler's data instead of re instantiating it
    Also some whitespace and styling changes

--- kano/profiling_late.py
+++ kano/profiling_late.py
@@ -15,43 +15,56 @@ from kano.logging import logger
 from kano.profiling import CONF_FILE
 
 # load the configuration file
-conf = yaml.load(open(CONF_FILE, "r"))
-myProfile=None
-app_name=sys.argv[0]
+with open(CONF_FILE, "r") as inp_conf:
+    conf = yaml.load(inp_conf)
+myProfile = cProfile.Profile()
+app_name = sys.argv[0]
 
-def has_key(d,k):
-    return type(d) is dict and d.has_key(k)
 
-def declare_timepoint(name,isStart):
+def has_key(d, k):
+    return type(d) is dict and k in d
+
+
+def declare_timepoint(name, isStart):
     global myProfile
-    cmd=None
-    pythonProfile=False
+    cmd = None
+    pythonProfile = False
 
-    if has_key(conf,app_name):
-        if has_key(conf[app_name],name):
-            ct=conf[app_name][name]
+    # Check if the app is contained in the profiling conf file
+    if has_key(conf, app_name):
+        # Check if the timepoint name is contained in the profiling conf file
+        if has_key(conf[app_name], name):
+            ct = conf[app_name][name]
 
-            # check if python profiler should be started for this timepoint
-            if has_key(ct,'python'):
-                pythonProfile=True
+            # Check if python profiler should be started for this timepoint
+            if has_key(ct, 'python'):
+                pythonProfile = True
                 if isStart:
-                    myProfile=cProfile.Profile()
                     myProfile.enable()
                 else:
                     if myProfile is None:
-                        logger.error(" timepoint "+name+" not started")
+                        logger.error(' timepoint '+name+' not started')
                     else:
                         myProfile.disable()
-                        myProfile.dump_stats(ct['python']['statfile'])
-                        myProfile=None
+                        # Check if the statfile location in spefified
+                        if ct['python']['statfile']:
+                            myProfile.dump_stats(ct['python']['statfile'])
+                            myProfile.clear()
+                        else:
+                            logger.error('No statfile entry in profiling conf file')
+            else:
+                logger.info('Profiling conf file doesnt enable the Python profiler for point {} at app {}'.format(name, app_name))
 
-            # check if we want to run some other command at this timepoint
-            if isStart and has_key(ct,'start_exec'):
-                cmd=ct['start_exec']
+            # Check if we want to run some other command at this timepoint
+            if isStart and has_key(ct, 'start_exec'):
+                cmd = ct['start_exec']
                 os.system(cmd)
-            if not isStart and has_key(ct,'end_exec'):
-                cmd=ct['end_exec']
+            if not isStart and has_key(ct, 'end_exec'):
+                cmd = ct['end_exec']
                 os.system(cmd)
+        else:
+            logger.info('Profiling conf file doesnt include point:{} for app {}'.format(name, app_name))
+    else:
+        logger.info('Profiling conf file doesnt include app:{}'.format(app_name))
 
-    logger.debug("timepoint "+name,transition=name,isStart=isStart,cmd=cmd,pythonProfile=pythonProfile)
-
+    logger.debug('timepoint '+name, transition=name, isStart=isStart, cmd=cmd, pythonProfile=pythonProfile)
