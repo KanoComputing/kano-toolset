594d2d38441c4474f359d328a83419d137c73fe9
Costas Voloudakis <costas@kano.me>
2015-02-20 17:49:24 +0000

    Whitespace and style changes
    Make flake8 happy

--- kano/profiling_late.py
+++ kano/profiling_late.py
@@ -16,26 +16,28 @@ from kano.profiling import CONF_FILE
 
 # load the configuration file
 conf = yaml.load(open(CONF_FILE, "r"))
-myProfile=None
-app_name=sys.argv[0]
+myProfile = None
+app_name = sys.argv[0]
 
-def has_key(d,k):
+
+def has_key(d, k):
     return type(d) is dict and d.has_key(k)
 
-def declare_timepoint(name,isStart):
-    global myProfile
-    cmd=None
-    pythonProfile=False
 
-    if has_key(conf,app_name):
-        if has_key(conf[app_name],name):
-            ct=conf[app_name][name]
+def declare_timepoint(name, isStart):
+    global myProfile
+    cmd = None
+    pythonProfile = False
+
+    if has_key(conf, app_name):
+        if has_key(conf[app_name], name):
+            ct = conf[app_name][name]
 
             # check if python profiler should be started for this timepoint
-            if has_key(ct,'python'):
-                pythonProfile=True
+            if has_key(ct, 'python'):
+                pythonProfile = True
                 if isStart:
-                    myProfile=cProfile.Profile()
+                    myProfile = cProfile.Profile()
                     myProfile.enable()
                 else:
                     if myProfile is None:
@@ -43,15 +45,14 @@ def declare_timepoint(name,isStart):
                     else:
                         myProfile.disable()
                         myProfile.dump_stats(ct['python']['statfile'])
-                        myProfile=None
+                        myProfile = None
 
             # check if we want to run some other command at this timepoint
-            if isStart and has_key(ct,'start_exec'):
-                cmd=ct['start_exec']
+            if isStart and has_key(ct, 'start_exec'):
+                cmd = ct['start_exec']
                 os.system(cmd)
-            if not isStart and has_key(ct,'end_exec'):
-                cmd=ct['end_exec']
+            if not isStart and has_key(ct, 'end_exec'):
+                cmd = ct['end_exec']
                 os.system(cmd)
 
-    logger.debug("timepoint "+name,transition=name,isStart=isStart,cmd=cmd,pythonProfile=pythonProfile)
-
+    logger.debug("timepoint "+name, transition=name, isStart=isStart, cmd=cmd, pythonProfile=pythonProfile)
