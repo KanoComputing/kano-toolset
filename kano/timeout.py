c50323a3af1a892762c87eebec86e0e99ba32015
Radek Pazdera <radek@kano.me>
2015-07-01 16:31:09 +0100

    Adding the timeout module

---
+++ kano/timeout.py
@@ -0,0 +1,40 @@
+# The timeout decorator
+#
+# Allows to run functions with a specified timeout easily.
+#
+# Source:
+#   http://stackoverflow.com/questions/2281850/timeout-function-if-it-takes-too-long-to-finish
+#
+# Usage:
+#
+#   @timeout(10) #seconds
+#   def your_function(a, b):
+#       ...
+#
+#   The function will be interrupted after the specified timeout
+
+from functools import wraps
+import errno
+import os
+import signal
+
+class TimeoutError(Exception):
+    pass
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
