bb153e868a5e8231435ada54cc68e42803ac090d
Tom Bettany <tom@kano.me>
2016-01-06 16:49:34 +0000

    Decorators: Add parameters to root decorator
    The decorator which enforces the root requirement has a few preferences
    for how it should behave: whether it should exit in this case and
    whether the user should be informed of the exit via stdout. To
    accommodate for this, add a decorator wrapper so that it can take
    parameters.

--- kano/decorators.py
+++ kano/decorators.py
@@ -7,14 +7,45 @@
 
 
 import os
+import sys
 from kano.logging import logger
 
-def require_root(func):
-    def ensure_root():
-        if not os.getuid() == 0:
-            logger.error('You need to run this option as root, try with sudo')
-            return False
+ERR_ROOT_PERMISSIONS_REQ = -1
 
-        return func()
 
-    return ensure_root
+def require_root(exit_on_failure=False, verbose=False):
+    '''
+    Generates decorator to enforce root permissions
+
+    NB: must be called when used as decorator, i.e.
+        @require_root()
+        def my_func():
+            pass
+
+    @params  exit_on_failure   Quit application on failure
+    @params  verbose           Print messages to stdout
+    '''
+
+    def require_root_decorator(func):
+        '''
+        Actual decorator that gets applied to functions
+        '''
+
+        def ensure_root(*args, **kwargs):
+            if os.getuid() != 0:
+                msg = 'You need to run this option as root, try with sudo'
+                logger.error(msg)
+
+                if verbose:
+                    print msg
+
+                if exit_on_failure:
+                    sys.exit(ERR_ROOT_PERMISSIONS_REQ)
+
+                return False
+
+            return func(*args, **kwargs)
+
+        return ensure_root
+
+    return require_root_decorator
