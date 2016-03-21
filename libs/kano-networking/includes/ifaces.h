14485b34616318c5752e42520b1baf4988f97955
Tom Bettany <tom@kano.me>
2016-03-21 09:59:16 +0000

    libkano_networking: Separate interface selection from returning name
    The function to retrieve the name of the selected interface gets the
    interface structure and returns only the name. Separate these two
    behaviours so that the interface structure can be reused.

--- libs/kano-networking/includes/ifaces.h
+++ libs/kano-networking/includes/ifaces.h
@@ -10,10 +10,16 @@
 #ifndef __KANO_NETWORKING_IFACES__
 #define __KANO_NETWORKING_IFACES__
 
+#include <ifaddrs.h>
+
 enum ERRORS {
-    E_NO_INTERFACES = -1
+    E_NO_INTERFACES = -1,
+    E_ALLOCATION_ERROR = -2
 };
 
+int sockaddr_cpy(struct sockaddr **const dest, const struct sockaddr *src);
+int ifaddrs_cpy(struct ifaddrs **const dest, const struct ifaddrs *src);
+int get_iface(const char *iface_type, struct ifaddrs **iface);
 int select_iface(const char *iface_type, char **iface_name);
 int check_iface_type(const char *iface_type);
 
--- libs/kano-networking/src/ifaces.c
+++ libs/kano-networking/src/ifaces.c
@@ -15,15 +15,73 @@
 
 #include "ifaces.h"
 
+
+/**
+ * Copy a 'struct sockaddr' instance
+ *
+ * The caller is responsible for freeing 'dest'
+ */
+int sockaddr_cpy(struct sockaddr **const dest, const struct sockaddr *src)
+{
+    if (src == NULL) {
+        *dest = NULL;
+        return 1;
+    }
+
+    *dest = malloc(sizeof(*src));
+
+    if (!*dest)
+        return E_ALLOCATION_ERROR;
+
+    memcpy(*dest, src, sizeof(**dest));
+
+    return memcmp(*dest, src, sizeof(*src)) != 0;
+}
+
+
+/**
+ * Perform a copy of a single 'struct ifaddrs' instance
+ *
+ * Note: Doesn't copy the linked 'struct ifaddrs' pointed to by 'ifa_next'
+ *
+ * The caller is responsible for freeing 'dest' with a 'freeifaddrs()' call
+ */
+int ifaddrs_cpy(struct ifaddrs **const dest, const struct ifaddrs *src)
+{
+    struct ifaddrs *dest_if = malloc(sizeof(*src));
+
+    if (!dest_if)
+        return E_ALLOCATION_ERROR;
+
+    // Only copy the instance so remove the linked iface
+    dest_if->ifa_next = NULL;
+
+    dest_if->ifa_name = strndup(src->ifa_name, strlen(src->ifa_name));
+    dest_if->ifa_flags = src->ifa_flags;
+
+    sockaddr_cpy(&dest_if->ifa_addr, src->ifa_addr);
+    sockaddr_cpy(&dest_if->ifa_netmask, src->ifa_netmask);
+    sockaddr_cpy(&dest_if->ifa_broadaddr, src->ifa_broadaddr);
+    sockaddr_cpy(&dest_if->ifa_dstaddr, src->ifa_dstaddr);
+
+    // Unsure how to handle 'void *ifa_data' - don't copy for now
+    dest_if->ifa_data = NULL;
+
+    *dest = dest_if;
+
+    return 0;
+}
+
+
 /**
  * Search through the available interfaces and return the first available
  * which matches the type provided
  *
- * The caller is responsible for freeing iface_name
+ * The caller is responsible for freeing iface
  *
- * Upon error, iface_name does not need freeing
+ * Upon error, iface does not need freeing
  */
-int select_iface(const char *iface_type, char **iface_name)
+int get_iface(const char *iface_type, struct ifaddrs **iface)
 {
     struct ifaddrs *iface_addr;
 
@@ -47,11 +105,35 @@ int select_iface(const char *iface_type, char **iface_name)
         return E_NO_INTERFACES;
     }
 
-    *iface_name = strdup(candidate_iface->ifa_name);
+    ifaddrs_cpy(iface, candidate_iface);
     freeifaddrs(iface_addr);
 
     return 0;
 }
+
+
+/**
+ * Search through the available interfaces and return the name of the first
+ * available which matches the type provided
+ *
+ * The caller is responsible for freeing iface_name
+ *
+ * Upon error, iface_name does not need freeing
+ */
+int select_iface(const char *iface_type, char **iface_name)
+{
+    struct ifaddrs *iface_addr;
+    int rv = get_iface(iface_type, &iface_addr);
+
+    if (rv < 0)
+        return rv;
+
+    *iface_name = strdup(iface_addr->ifa_name);
+    freeifaddrs(iface_addr);
+
+    return 0;
+}
+
 
 /**
  * Check for interfaces of the provided type and print the interface name
