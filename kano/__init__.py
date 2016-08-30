beaa9b9e9769c910561d28a52381794b35157641
Keith Eberlin <keith@kano.me>
2016-08-30 09:39:25 +0000

    Ensure the toolset translation strings are used when accessing this as a library.

--- kano/__init__.py
+++ kano/__init__.py
@@ -8,3 +8,16 @@
 
 __author__ = 'Kano Computing Ltd.'
 __email__ = 'dev@kano.me'
+
+import os, sys
+
+DIR_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if DIR_PATH != '/usr':
+    sys.path.insert(1, DIR_PATH)
+    LOCALE_PATH = os.path.join(DIR_PATH, 'locale')
+else:
+    LOCALE_PATH = None
+
+import kano_i18n.init
+kano_i18n.init.register_domain('kano-toolset', LOCALE_PATH)
+
