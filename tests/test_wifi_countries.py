4b2cd6411926e1ddc2d06d8010b00a0379859f3b
Tom Bettany <tom@kano.me>
2017-12-20 11:38:29 +0000

    Tests: Fix WiFi countries test to work with PyTest
    The WiFi countries test failed for every test case because it was
    written for Unittest and didn't run correctly when run with PyTest.
    Migrate to the more modern format.

--- tests/test_wifi_countries.py
+++ tests/test_wifi_countries.py
@@ -1,80 +1,156 @@
 #!/usr/bin/python
-
+#
 # test_wifi_countries.py
 #
 # Copyright (C) 2017 Kano Computing Ltd.
-# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 #
 # This module tests Wireless support for country codes
 #
 
+
 import os
-import sys
-import unittest
+import textwrap
+import subprocess
+import pytest
 
-sys.path.insert(0, '../')
-from kano import network
 
-test_essid='testessid'
-test_passphrase='testpassphrase'
-test_conffile='/tmp/test_wifi_countries.conf'
+TEST_NETWORK = {
+    'essid': 'testessid',
+    'psk': 'testpassphrase',
+    'wpa_passphrase': textwrap.dedent('''
+        network={
+                ssid="testessid"
+                #psk="testpassphrase"
+                psk=5a27f391c26458ddb9be0c347291a0ee1f419c41be9e48091cf0bc4e59b00813
+        }
+    ''').lstrip()
+}
+TEST_ESSID = 'testessid'
+TEST_CONFFILE = '/tmp/test_wifi_countries.conf'
 
-class TestWirelessCountries(unittest.TestCase):
 
-    def setUp(self):
-        self._set_envvar('KANO_WIFI_COUNTRY', '')
-        self._set_envvar('LANG', '')        
-    
-    def _set_envvar(self, envvar, value):
-        os.environ[envvar] = value
 
-    def _generate_conf(self):
-        if os.path.isfile(test_conffile):
-            os.unlink(test_conffile)
-        network.wpa_conf(test_essid, test_passphrase, test_conffile)
+@pytest.fixture(scope='function')
+def get_wpa_conf(monkeypatch, fs):
+    monkeypatch.setenv('KANO_WIFI_COUNTRY', '')
+    monkeypatch.setenv('LANG', '')
 
-    def _get_conf(self):
-        with open(test_conffile) as f:
-            a=f.readlines()
+    def fake_subprocess_check_output(cmd):
+        if type(cmd) != list:
+            return
 
-        for l in a:
-            if l.find('country=') != -1:
-                return l[l.find('country=') + 8:].strip()
+        if cmd[0] == 'wpa_passphrase':
+            return TEST_NETWORK.get('wpa_passphrase')
+
+
+    def get_conf():
+        if not os.path.exists(TEST_CONFFILE):
+            return None
+
+        with open(TEST_CONFFILE) as conf_f:
+            conf = conf_f.readlines()
+
+        for line in conf:
+            if line.find('country=') != -1:
+                return line[line.find('country=') + 8:].strip()
 
         return None
 
-    def test_country_lang_argentina(self):
-        # Mimic automatic detection for Argentinian locale
-        self._set_envvar('LANG', 'es_AR.UTF-8')
-        self._generate_conf()
-        self.assertEqual(self._get_conf(), 'AR', msg=self._get_conf())
+    monkeypatch.setattr(
+        subprocess, 'check_output',
+        fake_subprocess_check_output
+    )
 
-    def test_country_lang_spanish(self):
-        # Mimic automatic detection for Spanish locale
-        self._set_envvar('LANG', 'es_ES.UTF-8')
-        self._generate_conf()
-        self.assertEqual(self._get_conf(), 'ES', msg=self._get_conf())
+    conf_dir = os.path.dirname(TEST_CONFFILE)
+    if not fs.Exists(conf_dir):
+        fs.CreateDirectory(conf_dir)
 
-    def test_country_custom(self):
-        # Custom country code must override LANG automatic detection
-        self._set_envvar('LANG', 'en_US.UTF-8')
-        self._set_envvar('KANO_WIFI_COUNTRY', 'es_ES.UTF-8')
-        self._generate_conf()
-        self.assertEqual(self._get_conf(), 'ES', msg=self._get_conf())
-
-    def test_country_custom_malformed(self):
-        # Invalid format should not set any country
-        self._set_envvar('KANO_WIFI_COUNTRY', 'ez-ZZ-NOT-GOOD')
-        self._generate_conf()
-        self.assertEqual(self._get_conf(), None, msg=self._get_conf())
-
-    def test_country_lang_us(self):
-        # For US lang, we do not enforce wireless country code
-        self._set_envvar('LANG', 'en_US.UTF-8')
-        self._generate_conf()
-        self.assertNotEqual(self._get_conf(), 'US', msg=self._get_conf())
+    return get_conf
 
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_country_lang_argentina(monkeypatch, get_wpa_conf):
+    '''
+    Mimic automatic detection for Argentinian locale
+    '''
+
+    monkeypatch.setenv('LANG', 'es_AR.UTF-8')
+
+    from kano import network
+    assert network.wpa_conf(
+        TEST_NETWORK.get('essid'),
+        TEST_NETWORK.get('psk'),
+        TEST_CONFFILE
+    )
+
+    assert get_wpa_conf() == 'AR'
+
+
+def test_country_lang_spanish(monkeypatch, get_wpa_conf):
+    '''
+    Mimic automatic detection for Spanish locale
+    '''
+
+    monkeypatch.setenv('LANG', 'es_ES.UTF-8')
+
+    from kano import network
+    assert network.wpa_conf(
+        TEST_NETWORK.get('essid'),
+        TEST_NETWORK.get('psk'),
+        TEST_CONFFILE
+    )
+
+    assert get_wpa_conf() == 'ES'
+
+
+def test_country_custom(monkeypatch, get_wpa_conf):
+    '''
+    Custom country code must override LANG automatic detection
+    '''
+
+    monkeypatch.setenv('LANG', 'en_US.UTF-8')
+    monkeypatch.setenv('KANO_WIFI_COUNTRY', 'es_ES.UTF-8')
+
+    from kano import network
+    assert network.wpa_conf(
+        TEST_NETWORK.get('essid'),
+        TEST_NETWORK.get('psk'),
+        TEST_CONFFILE
+    )
+
+    assert get_wpa_conf() == 'ES'
+
+
+def test_country_custom_malformed(monkeypatch, get_wpa_conf):
+    '''
+    Invalid format should not set any country
+    '''
+
+    monkeypatch.setenv('KANO_WIFI_COUNTRY', 'ez-ZZ-NOT-GOOD')
+
+    from kano import network
+    assert network.wpa_conf(
+        TEST_NETWORK.get('essid'),
+        TEST_NETWORK.get('psk'),
+        TEST_CONFFILE
+    )
+
+    assert get_wpa_conf() is None
+
+
+def test_country_lang_us(monkeypatch, get_wpa_conf):
+    '''
+    For US lang, we do not enforce wireless country code
+    '''
+
+    monkeypatch.setenv('LANG', 'en_US.UTF-8')
+
+    from kano import network
+    assert network.wpa_conf(
+        TEST_NETWORK.get('essid'),
+        TEST_NETWORK.get('psk'),
+        TEST_CONFFILE
+    )
+
+    assert get_wpa_conf() != 'US'
