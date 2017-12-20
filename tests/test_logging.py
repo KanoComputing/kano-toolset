60d76f78aacf6332e4a6ee774c23df46cc8a3002
Tom Bettany <tom@kano.me>
2017-12-20 14:45:25 +0000

    Tests: Fix logging tests for PyTest
    The Unittest versions of the logging tests failed due to them now being
    run using PyTest. Prevent these failures by updating the tests.

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -8,19 +8,21 @@
 # This module tests the Kano logging module
 #
 
-import unittest
-from kano import logging as klm
 
 import os
 import sys
+import contextlib
 import tempfile
 import shutil
-sys.path.append('../')
+import unittest
+import pytest
 
 
 class TestNormaliseLevel(unittest.TestCase):
 
     def test_case_normalisation(self):
+        from kano import logging as klm
+
         ret = klm.normalise_level('INfo')
         self.assertEqual('info', ret)
 
@@ -34,10 +36,14 @@ class TestNormaliseLevel(unittest.TestCase):
         self.assertEqual('error', ret)
 
     def test_not_matching(self):
+        from kano import logging as klm
+
         ret = klm.normalise_level('random')
         self.assertEqual('none', ret)
 
     def test_aliases(self):
+        from kano import logging as klm
+
         for level in klm.LEVELS.keys():
             # create a list with aliases such as
             # ['d', 'de', 'deb', 'debu']
@@ -47,127 +53,170 @@ class TestNormaliseLevel(unittest.TestCase):
                 self.assertEqual(level, ret)
 
     def test_wrong_aliases(self):
+        from kano import logging as klm
+
         ret = klm.normalise_level('ing')
         self.assertEqual('none', ret)
 
 
-class TestForceFlush(unittest.TestCase):
-    def get_new_logger_instance(self):
-        self.logger = klm.Logger()
-        self.logger.set_app_name(sys.argv[0])
-        self.exp_log_fname = '{}.log'.format(self.logger._app_name)
-        self.exp_log_fname_full = os.path.join(self.temp_dir,
-                                               self.exp_log_fname)
+@pytest.fixture(scope='function')
+def new_logger(monkeypatch):
+    '''
+    Provides a function to get a new logger object at will, initialised with
+    the desired log and output levels.
 
-    def setUp(self):
-        os.environ[klm.LOG_ENV] = 'debug'
-        os.environ[klm.OUTPUT_ENV] = 'error'
+    Usage:
+        def test_something(new_logger):
+            with new_logger() as logger:
+                assert logger is not None
+    '''
 
-        self.temp_dir = tempfile.mkdtemp(dir='/tmp')
-        klm._test_override_logs_dir(self.temp_dir)
+    @contextlib.contextmanager
+    def logger_generator(log_level='debug', output_level='error'):
+        '''
+        Returns the logger object itself for testing
+        '''
 
-        self.data_from_log = {}
+        import kano
 
-        self.get_new_logger_instance()
+        # Patch the environment with the given log level
+        monkeypatch.setenv(kano.logging.LOG_ENV, log_level)
+        monkeypatch.setenv(kano.logging.OUTPUT_ENV, output_level)
+
+        # Initialise the logger after setting the environment variables
+        logger = kano.logging.Logger()
+        logger.set_app_name(sys.argv[0])
+
+        temp_dir = tempfile.mkdtemp()
+
+        log_file = '{}.log'.format(logger._app_name)
+        logger.log_file_path = os.path.join(
+            temp_dir, log_file
+        )
+
+        # The kano.logging module is lazy loaded using kano._logging so that is
+        # The module that needs patching
+        monkeypatch.setattr(kano._logging, 'SYSTEM_LOGS_DIR', temp_dir)
+        monkeypatch.setattr(kano._logging, 'USER_LOGS_DIR', temp_dir)
 
         # Prepare a few messages to write
-        self.msg_err_str = 'This is an error OH NO!'
-        self.msg_warn_str = 'Attention warning this is a warning'
-        self.msg_info_str = 'This is a piece of information'
-        self.msg_debug_str = 'I have no idea what is going on'
+        logger.msg_err_str = 'This is an error OH NO!'
+        logger.msg_warn_str = 'Attention warning this is a warning'
+        logger.msg_info_str = 'This is a piece of information'
+        logger.msg_debug_str = 'I have no idea what is going on'
 
-    def tearDown(self):
-        # We may want to set the env variable for force flushing, unset
-        # this for the other tests
-        if klm.FORCE_FLUSH_ENV in os.environ:
-            os.environ.pop(klm.FORCE_FLUSH_ENV)
+        def read_log_file():
+            '''
+            Helper for reading the log file associated with this test
+            '''
 
-        # Delete the folder where our logs were put
-        shutil.rmtree(self.temp_dir)
-        del self.logger
-        self.data_from_log.clear()
+            logs = kano.logging.read_logs()
+            print logs
+            assert logger.log_file_path in logs, \
+                'No log file was found for this app'
 
-    def get_data_from_log_file(self):
-        self.data_from_log = klm.read_logs()
-        self.assertIn(self.exp_log_fname_full,
-                      self.data_from_log,
-                      msg='No log file was found for this app')
+            return logs[logger.log_file_path]
 
-    def check_if_messages_are_in(self, msgs):
-        ''' Checks if the standard messages are in the message list
-        that is given in as a list() instance
-        '''
-        self.assertIn(self.msg_err_str,
-                      msgs,
-                      msg='The error message put in was not found')
-        self.assertIn(self.msg_warn_str,
-                      msgs,
-                      msg='The warn message put in was not found')
-        self.assertIn(self.msg_info_str,
-                      msgs,
-                      msg='The info message put in was not found')
-        self.assertIn(self.msg_debug_str,
-                      msgs,
-                      msg='The debug message put in was not found')
+        logger.read_log_file = read_log_file
 
-    def test_regular_write(self):
-        ''' Print a message for each of the levels, close the log
-        file and check whether it is included in the log file
-        '''
-        self.logger.error(self.msg_err_str)
-        self.logger.warn(self.msg_warn_str)
-        self.logger.info(self.msg_info_str)
-        self.logger.debug(self.msg_debug_str)
+        def check_if_messages_are_in(msgs):
+            '''
+            Checks if the standard messages are in the message list
+            that is given in as a list() instance
+            '''
+
+            assert logger.msg_err_str in msgs, \
+                'The error message put in was not found'
+            assert logger.msg_warn_str in msgs, \
+                'The warn message put in was not found'
+            assert logger.msg_info_str in msgs, \
+                'The info message put in was not found'
+            assert logger.msg_debug_str in msgs, \
+                'The debug message put in was not found'
+
+        logger.check_if_messages_are_in = check_if_messages_are_in
+
+        yield logger
+
+        shutil.rmtree(temp_dir)
+
+    return logger_generator
+
+
+def test_regular_write(new_logger):
+    ''' Print a message for each of the levels, close the log
+    file and check whether it is included in the log file
+    '''
+
+    with new_logger() as logger:
+        logger.error(logger.msg_err_str)
+        logger.warn(logger.msg_warn_str)
+        logger.info(logger.msg_info_str)
+        logger.debug(logger.msg_debug_str)
+
         # flush flushes and closes the file
-        self.logger.flush()
-        self.get_data_from_log_file()
-        entries = self.data_from_log[self.exp_log_fname_full]
-        msgs = [a['message'] for a in entries]
-        self.check_if_messages_are_in(msgs)
+        logger.flush()
 
-    def test_force_flush_flag(self):
-        ''' Print a message for each of the levels, but force flush
-        it to the log file through using the force_flush flag argument
-        '''
-        self.logger.error(self.msg_err_str, force_flush=True)
-        self.logger.warn(self.msg_warn_str, force_flush=True)
-        self.logger.info(self.msg_info_str, force_flush=True)
-        self.logger.debug(self.msg_debug_str, force_flush=True)
-        self.get_data_from_log_file()
-        entries = self.data_from_log[self.exp_log_fname_full]
-        msgs = [a['message'] for a in entries]
-        self.check_if_messages_are_in(msgs)
+        entries = logger.read_log_file()
 
-    def test_force_flush_env(self):
-        ''' Print a message for each of the levels, but force flush
-        it to the log file through using the force_flush env var
-        '''
-        os.environ[klm.FORCE_FLUSH_ENV] = '1'
-        # We need to get a new instance since we changed the envs
-        self.get_new_logger_instance()
-        self.logger.error(self.msg_err_str)
-        self.logger.warn(self.msg_warn_str)
-        self.logger.info(self.msg_info_str)
-        self.logger.debug(self.msg_debug_str)
-        self.get_data_from_log_file()
-        entries = self.data_from_log[self.exp_log_fname_full]
         msgs = [a['message'] for a in entries]
-        self.check_if_messages_are_in(msgs)
+        logger.check_if_messages_are_in(msgs)
 
-    def test_force_flush_fns(self):
-        ''' Print a message for each of the levels, but force flush
-        it to the log file through using the force_flush class functions
-        '''
-        self.logger.set_force_flush()
-        self.logger.error(self.msg_err_str)
-        self.logger.warn(self.msg_warn_str)
-        self.logger.info(self.msg_info_str)
-        self.logger.debug(self.msg_debug_str)
-        self.logger.unset_force_flush()
-        self.get_data_from_log_file()
-        entries = self.data_from_log[self.exp_log_fname_full]
+
+def test_force_flush_flag(new_logger):
+    ''' Print a message for each of the levels, but force flush
+    it to the log file through using the force_flush flag argument
+    '''
+
+    with new_logger() as logger:
+        logger.error(logger.msg_err_str, force_flush=True)
+        logger.warn(logger.msg_warn_str, force_flush=True)
+        logger.info(logger.msg_info_str, force_flush=True)
+        logger.debug(logger.msg_debug_str, force_flush=True)
+
+        entries = logger.read_log_file()
+
         msgs = [a['message'] for a in entries]
-        self.check_if_messages_are_in(msgs)
+        logger.check_if_messages_are_in(msgs)
 
-if __name__ == '__main__':
-    unittest.main()
+
+def test_force_flush_env(new_logger, monkeypatch):
+    ''' Print a message for each of the levels, but force flush
+    it to the log file through using the force_flush env var
+    '''
+
+    from kano import logging
+
+    monkeypatch.setenv(logging.FORCE_FLUSH_ENV, '1')
+    # We need to get a new instance since we changed the envs
+
+    with new_logger() as logger:
+        logger.error(logger.msg_err_str)
+        logger.warn(logger.msg_warn_str)
+        logger.info(logger.msg_info_str)
+        logger.debug(logger.msg_debug_str)
+
+        entries = logger.read_log_file()
+
+        msgs = [a['message'] for a in entries]
+        logger.check_if_messages_are_in(msgs)
+
+
+def test_force_flush_fns(new_logger):
+    ''' Print a message for each of the levels, but force flush
+    it to the log file through using the force_flush class functions
+    '''
+
+    with new_logger() as logger:
+        logger.set_force_flush()
+
+        logger.error(logger.msg_err_str)
+        logger.warn(logger.msg_warn_str)
+        logger.info(logger.msg_info_str)
+        logger.debug(logger.msg_debug_str)
+        logger.unset_force_flush()
+
+        entries = logger.read_log_file()
+
+        msgs = [a['message'] for a in entries]
+        logger.check_if_messages_are_in(msgs)
